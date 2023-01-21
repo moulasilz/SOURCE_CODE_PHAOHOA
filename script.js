804e530fd2529aa791d472ebaddfd86abe02b888
moulasilz <qzarsixx@gmail.com>
2023-01-21 20:26:42 +0700

    ok

--- script.js
+++ script.js
@@ -2292,7 +2292,7 @@ if (IS_HEADER) {
 	init();
 } else {
 	// Allow status to render, then preload assets and start app.
-	setLoadingStatus('Lighting Fuses');
+	setLoadingStatus('Lên pháo');
 	setTimeout(() => {
 		soundManager.preload()
 		.then(
